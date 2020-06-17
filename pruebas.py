675ab6f166f0a3ba89cce2bb3d7b43193d583b04
arielmerin <arielmerino@ciencias.unam.mx>
2020-06-17 14:14:21 -0500

    Operadores del lenguaje

--- pruebas.py
+++ pruebas.py
@@ -1,26 +1,16 @@
-print(3)
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+# This is a commit
+a = 5
 
+print("La variable a: ", a, " es del tipo: ", type(a))
+lista = [1,1,2,3,5,8,13]
+tupla = (2,4,6,8)
 
+print("Est es una lista", lista)
+a = 54
+b = 2
+c = a % 
+print("El residuo de: ", a, " y ", b, " es: ", c)
+v, b = 3.1, 4.2
+print(type(v))
+print("la suma de 3.1 y 4.3 es: ", v + b)
 
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
-
-
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
-
-
-
-esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
-
-
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na op
-esta es na oesta es na oesta es na oesta es na opsta es na op
