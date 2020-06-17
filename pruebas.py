ce64dd8d8651383df03c565d874e1fa9c8800280
arielmerin <arielmerino@ciencias.unam.mx>
2020-06-17 12:43:08 -0500

    First Commit

---
+++ pruebas.py
@@ -0,0 +1,26 @@
+print(3)
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+
+
+
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+
+
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+
+
+
+esta es na oesta es na oesta es na oesta es na opesta es na oesta es na oesta es na oesta es na op
+
+
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na op
+esta es na oesta es na oesta es na oesta es na opsta es na op
